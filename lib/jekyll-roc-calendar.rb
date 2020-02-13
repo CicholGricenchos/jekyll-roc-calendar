a65b08f6bc1f60ffca1ce5d7ffb7f48a21c34bb8
cichol <cichol@live.cn>
2020-02-13 19:36:42 -0400

    use chinese number

--- lib/jekyll-roc-calendar.rb
+++ lib/jekyll-roc-calendar.rb
@@ -1,7 +1,31 @@
 module Jekyll
   module RocCalendarFilter
+    DIGITS = %w{〇 一 二 三 四 五 六 七 八 九}
+
+    def self.year_number integer
+      integer.digits.reverse.map{|x| DIGITS[x]}.join
+    end
+
+    def self.month_number integer
+      unit, ten = integer.digits
+      if ten.nil?
+        ten = ''
+      elsif ten == 1
+        ten = '十'
+      else
+        ten = DIGITS[ten] + '十'
+      end
+      unit = DIGITS[unit]
+      ten + unit
+    end
+
     def roc_calendar(date)
-      "民國#{date.year - 1911}年#{date.month}月#{date.day}日" if date
+      if date
+        year = RocCalendarFilter.year_number date.year - 1911
+        month = RocCalendarFilter.month_number date.month
+        day = RocCalendarFilter.month_number date.day
+        "民國#{year}年#{month}月#{day}日"
+      end
     end
   end
 end
